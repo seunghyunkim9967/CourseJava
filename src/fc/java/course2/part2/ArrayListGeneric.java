75b717699e9465cd68059704bfb8a500592e6238
dnfheh <dnfheh@naver.com>
2024-03-21 20:39:03 +0900

    20240321

---
+++ src/fc/java/course2/part2/ArrayListGeneric.java
@@ -0,0 +1,15 @@
+package fc.java.course2.part2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListGeneric {
+    public static void main(String[] args) {
+//        List<Movie> list = new ArrayList();
+//        List.add(new Movie("괴물","봉준호","2006","한국"));
+//        list.add("ghello");
+
+
+    }
+
+}
