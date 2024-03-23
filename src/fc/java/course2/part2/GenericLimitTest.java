54495cd8ca4fdbd086d5568555f14805071a0922
dnfheh <dnfheh@naver.com>
2024-03-23 09:07:40 +0900

    20240323 제네릭 끝

---
+++ src/fc/java/course2/part2/GenericLimitTest.java
@@ -0,0 +1,19 @@
+package fc.java.course2.part2;
+
+import fc.java.model2.AverageCalculator;
+
+public class GenericLimitTest {
+    public static void main(String[] args) {
+        Integer[] integers = {1,2,3,4,5};
+        Double[] doubles = {1.0,2.0,3.0,4.0,5.0};
+
+        AverageCalculator<Integer> integerAverageCalculator = new AverageCalculator<>(integers);
+        double integerAverage = integerAverageCalculator.calculateAverage();
+        System.out.println(integerAverage);
+
+        AverageCalculator<Double> doubleAverageCalculator = new AverageCalculator<>(doubles);
+        double doubleAverage = integerAverageCalculator.calculateAverage();
+        System.out.println(doubleAverage);
+
+    }
+}
